4111d8f86a618d4b4e8840387804456c5d2798fa
Alfredo Deza <alfredo@deza.pe>
2022-04-26 21:22:26 -0400

    add app.py

---
+++ webapp/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def two_hundred():
+    return "200!"
+
+@app.route("/error")
+def error():
+    abort(500, "oh no some error!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=80, host="0.0.0.0")
